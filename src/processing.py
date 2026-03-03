246c101463f6198036e1700fa6e03e0f1ad31a4e
milkabubu <milkabu264@gmail.com>
2026-03-03 18:27:43 +0300

    Вторая функция написана

--- src/processing.py
+++ src/processing.py
@@ -15,3 +15,24 @@ def filter_by_state(data, state='EXECUTED'):
             result.append(item)
     return result
 
+
+def sort_by_date(data, reverse=True):
+    """
+    Сортирует список словарей по дате.
+
+    Параметры:
+    data (list): Список словарей с данными
+    reverse (bool):
+        - True (по умолчанию) - сортировка по убыванию (сначала новые)
+        - False - сортировка по возрастанию (сначала старые)
+
+    Возвращает:
+    list: Новый отсортированный список
+    """
+    # Копируем список, чтобы не изменять исходный
+    sorted_data = data.copy()
+
+    # Сортируем по ключу 'date'
+    sorted_data.sort(key=lambda x: x['date'], reverse=reverse)
+
+    return sorted_data
